a473510a9d8343b2401a5169a5d5305374f5c1cc
henk3520 <113079538+henk3520@users.noreply.github.com>
2024-01-12 14:51:57 +0800

    Create 暴力.py

---
+++ hw4/暴力.py
@@ -0,0 +1,20 @@
+def brute_force_solver():
+    epsilon = 0.0001
+    lower_bound = -10
+    upper_bound = 10
+
+    solutions = []
+
+    for x in range(lower_bound, upper_bound + 1):
+        equation_value = x**2 - 3*x + 1
+        if abs(equation_value) < epsilon:
+            solutions.append(x)
+
+    return solutions
+
+solutions = brute_force_solver()
+
+if solutions:
+    print("解為：", solutions)
+else:
+    print("未找到解")
